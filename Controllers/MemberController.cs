03df637f7ef00a0e8a39c2a1e04030361ce828f5
Halil Taha Basegmez <tahabasegmez@gmail.com>
2026-04-02 00:05:39 +0300

    feat(mappers): add default DTO-Entity mappers for every basic model

--- Controllers/MemberController.cs
+++ Controllers/MemberController.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
 using AssociationWebAPI.Domain.Entities;
 using AssociationWebAPI.Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-
 using AssociationWebAPI.Application.DTOs;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Internal;
-using AutoMapper;
-using AssociationWebAPI.Controllers;
+using AssociationWebAPI.Application.Mappers;
 
 namespace AssociationWebAPI.Controllers
 {
@@ -41,25 +35,7 @@ namespace AssociationWebAPI.Controllers
                 return NotFound();
             }
 
-            var memberDto = new CorporateResponseDto
-            {
-                CorporateName = member is Corporate corporate ? corporate.CorporateName : string.Empty,
-                RegistrationDate = member.RegistrationDate,
-                Status = member.Status,
-                InactiveDate = member.InactiveDate,
-                Email = member.Email,
-                Phone = member.Phone,
-                Address = new AddressResponseDto
-                {
-                    OpenAddress = member.Address.OpenAddress,
-                    DistrictId = member.Address.DistrictId,
-                    CityId = member.Address.CityId,
-                    StateId = member.Address.StateId,
-                    PostalCode = member.Address.PostalCode,
-                    CountryId = member.Address.CountryId
-                },
-                RepresentativeIds = member.Representatives.Select(r => r.Id).ToList()
-            };
+            var memberDto = DefaultDtoEntityMapperExtensions.ToResponseDto(member);
             
             return Ok(memberDto);
         }
@@ -78,30 +54,7 @@ namespace AssociationWebAPI.Controllers
                 return NotFound();
             }
 
-            var memberDto = new IndividualResponseDto
-            {
-                Name = member.Name,
-                Surname = member.Surname,
-                NationalIdentityNumber = member.NationalIdentityNumber,
-                Occupation = member.Occupation,
-                BirthDate = member.BirthDate,
-                Birthplace = member.Birthplace,
-                EducationStatus = member.EducationStatus,
-                RegistrationDate = member.RegistrationDate,
-                Status = member.Status,
-                InactiveDate = member.InactiveDate,
-                Email = member.Email,
-                Phone = member.Phone,
-                Address = new AddressResponseDto
-                {
-                    OpenAddress = member.Address.OpenAddress,
-                    DistrictId = member.Address.DistrictId,
-                    CityId = member.Address.CityId,
-                    StateId = member.Address.StateId,
-                    PostalCode = member.Address.PostalCode,
-                    CountryId = member.Address.CountryId
-                }
-            };
+            var memberDto = DefaultDtoEntityMapperExtensions.ToResponseDto(member);
 
             return Ok(memberDto);
         }
@@ -109,35 +62,11 @@ namespace AssociationWebAPI.Controllers
         [HttpPost("individual")]
         public IActionResult CreateIndividualMember([FromBody] IndividualRequestDto memberDto)
         {
-            var member = new Individual
-            {
-                RegistrationDate = memberDto.RegistrationDate,
-                Status = memberDto.Status,
-                InactiveDate = memberDto.InactiveDate,
-                Email = memberDto.Email,
-                Phone = memberDto.Phone,
-                Type = Domain.Enums.MemberType.Individual,
-                Address = new Address
-                {
-                    OpenAddress = memberDto.Address.OpenAddress,
-                    DistrictId = memberDto.Address.DistrictId,
-                    CityId = memberDto.Address.CityId,
-                    StateId = memberDto.Address.StateId,
-                    PostalCode = memberDto.Address.PostalCode,
-                    CountryId = memberDto.Address.CountryId,
-                },
-                Name = memberDto.Name,
-                Surname = memberDto.Surname,
-                NationalIdentityNumber = memberDto.NationalIdentityNumber,
-                Occupation = memberDto.Occupation,
-                BirthDate = memberDto.BirthDate,
-                Birthplace = memberDto.Birthplace,
-                EducationStatus = memberDto.EducationStatus
-            };
+            var member = DefaultDtoEntityMapperExtensions.ToEntity(memberDto);
 
             _context.Members.Add(member); 
             _context.SaveChanges();
-            return CreatedAtAction("GetIndividualMember", new { id = member.Id }, member);
+            return CreatedAtAction(nameof(GetIndividualMember), new { id = member.Id }, member);
         }
     }
 }
